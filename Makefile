7d4d24877815869d75481738f31fdbeeb89930ba
Wen Congyang <wency@cn.fujitsu.com>
2014-09-05 11:29:28 +0800

    add default compare ops for udp
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-obj-m := sch_colo.o sch_colo_arp.o sch_colo_ipv4.o sch_colo_icmp.o
+obj-m := sch_colo.o sch_colo_arp.o sch_colo_ipv4.o sch_colo_icmp.o \
+	 sch_colo_udp.o
 
 sch_colo-objs := colo.o connections.o ipv4_fragment.o ip_fragment.o \
 		 compare.o compare_device.o
@@ -7,6 +8,7 @@ sch_colo-objs := colo.o connections.o ipv4_fragment.o ip_fragment.o \
 sch_colo_arp-objs := compare_arp.o
 sch_colo_ipv4-objs := compare_ipv4.o
 sch_colo_icmp-objs := compare_icmp.o
+sch_colo_udp-objs := compare_udp.o
 
 KERNELBUILD := /lib/modules/`uname -r`/build
 default:
---
+++ compare_udp.c
@@ -0,0 +1,49 @@
+/*
+ *  COarse-grain LOck-stepping Virtual Machines for Non-stop Service (COLO)
+ *  (a.k.a. Fault Tolerance or Continuous Replication)
+ *  Compare udp packets from master and slave.
+ *
+ * Copyright (C) 2014 FUJITSU LIMITED
+ *
+ * Author: Wen Congyang <wency@cn.fujitsu.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "compare.h"
+#include "compare_ipv4.h"
+
+static void debug_print_udp(const struct compare_info *info, const void *data,
+			    int length)
+{
+	const struct udphdr *udp = data;
+	unsigned short src_port, dst_port;
+
+	src_port = ntohs(udp->source);
+	dst_port = ntohs(udp->dest);
+	pr_warn("HA_compare:[UDP] src=%u, dst=%u\n", src_port, dst_port);
+}
+
+static ipv4_compare_ops_t udp_ops = {
+	.debug_print = debug_print_udp,
+};
+
+static int __init compare_udp_init(void)
+{
+	return register_ipv4_compare_ops(&udp_ops, IPPROTO_UDP);
+}
+
+static void __exit compare_udp_fini(void)
+{
+	unregister_ipv4_compare_ops(&udp_ops, IPPROTO_UDP);
+}
+
+module_init(compare_udp_init);
+module_exit(compare_udp_fini);
+MODULE_LICENSE("GPL");
+MODULE_INFO(intree, "Y");
